4d12bb4f854ca6f738fb99330df96b41e861c57f
DESKTOP-5FEF2FS <ricardopablo.deleon@gmail.com>
2018-07-06 23:36:35 +0800

    Add application and model

---
+++ app/src/main/java/com/example/dicdicjalique/realmtest/model/Book.java
@@ -0,0 +1,57 @@
+package com.example.dicdicjalique.realmtest.model;
+
+import io.realm.annotations.PrimaryKey;
+
+public class Book {
+
+    @PrimaryKey
+    private int id;
+
+    private String title;
+
+    private String description;
+
+    private String author;
+
+    private String imageUrl;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
